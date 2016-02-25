64360626af4101ff00c23b7a0c00fdc788df577d
Andy Clement <aclement@pivotal.io>
2016-02-24 17:48:05 -0800

    Getting started, forked from the groovy transform processor
    This first variant actually allows directly passing the
    script rather than passing the name of the script file.

---
+++ src/main/java/org/springframework/cloud/stream/module/transform/ScriptTransformProcessor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.module.transform;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.module.common.ScriptVariableGeneratorConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.groovy.GroovyScriptExecutingMessageProcessor;
+import org.springframework.integration.handler.MessageProcessor;
+import org.springframework.integration.scripting.ScriptVariableGenerator;
+import org.springframework.scripting.ScriptSource;
+import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.scripting.support.StaticScriptSource;
+
+/**
+ * A Processor module that transforms messages using a supplied script.
+ *
+ * @author Eric Bottard
+ * @author Mark Fisher
+ * @author Marius Bogoevici
+ * @author Andy Clement
+ */
+@EnableBinding(Processor.class)
+@Import(ScriptVariableGeneratorConfiguration.class)
+@EnableConfigurationProperties(ScriptTransformProcessorProperties.class)
+public class ScriptTransformProcessor {
+
+	@Autowired
+	private ScriptVariableGenerator scriptVariableGenerator;
+
+	@Autowired
+	private ScriptTransformProcessorProperties properties;
+
+	@Bean
+	@Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	public MessageProcessor<?> transformer() {
+		String quotedScript = properties.getScript();
+		ScriptSource scriptSource = new StaticScriptSource(quotedScript.substring(1,quotedScript.length()-1),"Gscript");
+		try {
+			System.out.println(scriptSource.getScriptAsString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new GroovyScriptExecutingMessageProcessor(scriptSource, scriptVariableGenerator);
+	}
+
+}
---
+++ src/main/java/org/springframework/cloud/stream/module/transform/ScriptTransformProcessorProperties.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.module.transform;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+
+/**
+ * Configuration properties for the Groovy Transform Processor module.
+ *
+ * @author Eric Bottard
+ * @author Andy Clement
+ */
+@ConfigurationProperties
+public class ScriptTransformProcessorProperties {
+
+	private String language;
+	
+	private String script;
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	@NotNull
+	public String getScript() {
+		return script;
+	}
+
+	public void setScript(String script) {
+		this.script = script;
+	}
+}
